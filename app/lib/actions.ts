dc6a18e857b8e67fe5be181c5770d8ec200b2a39
Adrienn Pozsgai <apozsgai97@gmail.com>
2024-11-10 20:09:03 +0100

    add error.tsx and not-found.tsx for error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -42,7 +42,6 @@ export async function createInvoice(formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-
 export async function updateInvoice(id: string, formData: FormData) {
   const { customerId, amount, status } = UpdateInvoice.parse({
     customerId: formData.get("customerId"),
